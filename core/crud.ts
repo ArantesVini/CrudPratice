6c385d25fe872665ed4d454bf7c5a157b7bf8ea5
ArantesVini <vinicius.daniel02@gmail.com>
2023-06-02 12:23:29 -0300

    fix: change to typescript

--- core/crud.ts
+++ core/crud.ts
@@ -1,12 +1,13 @@
-const fs = require("fs");
+// const fs = require("fs");
+import fs from  "fs";
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content) {
+function create(content: string) {
     // save content on system
     fs.writeFileSync(DB_FILE_PATH, content)
     return content;
 }
 
-console.log(create("Creating some content, now with nodemon!"));
+console.log(create("Creating some content, now with Typescript!"));
