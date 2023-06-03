a0fe5978f210a5a552c7290aea11554ca5b06517
ArantesVini <vinicius.daniel02@gmail.com>
2023-06-03 10:31:25 -0300

    feat: Creation of the read function to get the information of the Array of TODOs, and CLEAR_DB fun.

--- core/crud.ts
+++ core/crud.ts
@@ -1,13 +1,53 @@
 // const fs = require("fs");
-import fs from  "fs";
+import fs from "fs";
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content: string) {
-    // save content on system
-    fs.writeFileSync(DB_FILE_PATH, content)
-    return content;
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
 }
 
-console.log(create("Creating some content, now with Typescript!"));
+function create(content: string) {
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+  const todos: Array<Todo> = [...read(), todo];
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2,
+    ),
+  );
+  return content;
+}
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+  if (!db.todos) {
+    return [];
+  }
+  return db.todos;
+}
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+// Create
+CLEAR_DB();
+create("First TODO;");
+create("Second TODO;");
+// Read
+console.log(read());
+// Update
+
+// Delete
